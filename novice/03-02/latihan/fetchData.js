1a704dcfcfc52962462a33621c25a1816503524d
mgilang13 <muhammad.gilang.n.k@gmail.com>
2019-12-17 16:06:47 +0700

    Penambahan File latihan 03-02

---
+++ novice/03-02/latihan/fetchData.js
@@ -0,0 +1,11 @@
+const fetch = require("node-fetch");
+fetch("https://jsonplaceholder.typicode.com/users/")
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
