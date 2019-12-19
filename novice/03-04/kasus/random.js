7c315d4882d754e17e949e868123f442f94db0c0
mgilang13 <muhammad.gilang.n.k@gmail.com>
2019-12-19 23:00:21 +0700

    Penambahan file kasus 03-04

---
+++ novice/03-04/kasus/random.js
@@ -0,0 +1,30 @@
+$(function() {
+  let rows = 20;
+  let cols = 10;
+  let cells = [];
+  function createGrid(root) {
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+        let cell = $("<div>").addClass("cell");
+        cells.push(cell);
+        root.append(cell);
+      }
+    }
+  }
+
+  let updateState = function() {
+    cells.forEach(function(cell) {
+      let randomNum = Math.random() * 2;
+      if (randomNum > 1) {
+        cell.addClass("active");
+      } else {
+        cell.removeClass("active");
+      }
+    });
+  };
+
+  setInterval(updateState, 500);
+
+  let $root = $(".container");
+  createGrid($root);
+});
